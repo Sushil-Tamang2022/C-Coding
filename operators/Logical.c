aad78ae9c4e82ec4c6dbf7fd3a6804fcda1d756a
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-20 00:25:29 +0545

    Logical Operator Added

---
+++ operators/Logical.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+#include <stdbool.h> 
+
+int main() {
+
+    // logical operators = Used to combine or modify boolean expressions.
+
+    // && = AND
+    // || = OR
+    // ! = NOT
+
+    int temp = 20;
+    bool isSunny = true;
+
+    if(temp > 0 && temp < 30){
+        printf("The temperature is GOOD");
+    }
+    else{
+        printf("The temperature is BAD");
+    }
+
+    if(!isSunny){
+        printf("It is CLOUDY outside");
+    }
+    else{
+        printf("It is SUNNY outside");
+    }
+
+    return 0;
+}
