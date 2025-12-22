712146c333de518cc74a36995c0d632d4cbe7078
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-22 06:10:20 +0545

    Adding Array

---
+++ Array/ArrayExample.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    // array = A fixed-size collection of elements of the same data type
+    //              (Similar to a variable, but it holds more than 1 value)
+
+    int numbers[] = {10, 20, 30, 40, 50, 60};
+    char grades[] = {'A', 'B', 'C', 'D', 'F'};
+    char name[] = "Bro Code";
+
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+
+    for(int i = 0; i < size; i++){
+        printf("%d ", numbers[i]);
+    }
+    
+    return 0;
+}
+
