a07f6f5a23891445ce0dbcc66dd8e68258b91f05
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-22 06:08:35 +0545

    Banking Program

--- Game/RockPaperScissors.c
+++ Game/RockPaperScissors.c
@@ -6,39 +6,42 @@ int getComputerChoice();
 int getUserChoice();
 void checkWinner(int userChoice, int computerChoice);
 
-int main() {
+int main()
+{
 
     // ROCK PAPER SCISSORS
 
     srand(time(NULL));
 
     printf("*** ROCK PAPER SCISSORS ***\n");
-    
+
     int userChoice = getUserChoice();
     int computerChoice = getComputerChoice();
 
-    switch(userChoice){
-        case 1:
-            printf("You chose ROCK!\n");
-            break;
-        case 2:
-            printf("You chose PAPER!\n");
-            break;
-        case 3:
-            printf("You chose SCISSORS!\n");
-            break;
+    switch (userChoice)
+    {
+    case 1:
+        printf("You chose ROCK!\n");
+        break;
+    case 2:
+        printf("You chose PAPER!\n");
+        break;
+    case 3:
+        printf("You chose SCISSORS!\n");
+        break;
     }
 
-    switch(computerChoice){
-        case 1:
-            printf("Computer chose ROCK!\n");
-            break;
-        case 2:
-            printf("Computer chose PAPER!\n");
-            break;
-        case 3:
-            printf("Computer chose SCISSORS!\n");
-            break;
+    switch (computerChoice)
+    {
+    case 1:
+        printf("Computer chose ROCK!\n");
+        break;
+    case 2:
+        printf("Computer chose PAPER!\n");
+        break;
+    case 3:
+        printf("Computer chose SCISSORS!\n");
+        break;
     }
 
     checkWinner(userChoice, computerChoice);
@@ -46,37 +49,44 @@ int main() {
     return 0;
 }
 
-int getComputerChoice(){
+int getComputerChoice()
+{
     return (rand() % 3) + 1;
 }
 
-int getUserChoice(){
+int getUserChoice()
+{
 
     int choice = 0;
 
-    do{
+    do
+    {
         printf("Choose an option\n");
         printf("1. ROCK\n");
         printf("2. PAPER\n");
         printf("3. SCISSORS\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-    }while(choice < 1 || choice > 3);
+    } while (choice < 1 || choice > 3);
 
     return choice;
 }
 
-void checkWinner(int userChoice, int computerChoice){
+void checkWinner(int userChoice, int computerChoice)
+{
 
-    if(userChoice == computerChoice){
+    if (userChoice == computerChoice)
+    {
         printf("It's a TIE!");
     }
-    else if((userChoice == 1 && computerChoice == 3) || 
-            (userChoice == 2 && computerChoice == 1) ||
-            (userChoice == 3 && computerChoice == 2)){
+    else if ((userChoice == 1 && computerChoice == 3) ||
+             (userChoice == 2 && computerChoice == 1) ||
+             (userChoice == 3 && computerChoice == 2))
+    {
         printf("You WIN!");
     }
-    else{
+    else
+    {
         printf("You LOSE!");
     }
 }
