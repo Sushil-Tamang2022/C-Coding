b5298eb37f4dac5d5cffd9705de6032ff7b2ab68
Sushil-Tamang2022 <sushiltamang411@gmail.com>
2025-12-23 08:30:07 +0545

    Enum Added

---
+++ Enum.c
@@ -0,0 +1,56 @@
+// ---------- EXAMPLE 1 ----------
+
+#include <stdio.h>
+
+typedef enum{
+    SUNDAY = 1, MONDAY = 2, TUESDAY = 3, WEDNESDAY = 4, 
+    THURSDAY = 5, FRIDAY = 6, SATURDAY = 7
+}Day;
+
+int main() {
+
+    Day today = TUESDAY;
+
+    if(today == SUNDAY || today == SATURDAY){
+        printf("It's the weekend");
+    }
+    else{
+        printf("It's a weekday");
+    }
+
+    return 0;
+}
+
+// ---------- EXAMPLE 2 ----------
+
+#include <stdio.h>
+
+typedef enum{
+    SUCCESS, FAILURE, PENDING
+}Status;
+
+void connectStatus(Status status);
+
+int main() {
+
+    Status status = PENDING;
+
+    connectStatus(status);
+
+    return 0;
+}
+
+void connectStatus(Status status){
+   
+    switch(status){
+        case SUCCESS:
+            printf("Connection was successful\n");
+            break;
+        case FAILURE:
+            printf("Could not connect\n");
+            break;
+        case PENDING:
+            printf("Connecting...\n");
+            break;
+    }
+}
